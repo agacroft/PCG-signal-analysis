85e1700e1e58ad912a3ca758e0c07e14de19c576
agnieszka-ka <agus.kaczmarczyk@gmail.com>
2015-07-20 12:56:52 +0200

    Added reading parameters and plotting method to wave_operations.py.

--- wave_operataions.py
+++ wave_operataions.py
@@ -5,3 +5,33 @@ Script for basic operations on wave files.
 @author: Agnieszka Kaczmarczyk
 """
 
+import wave
+import matplotlib.pyplot as plt
+import numpy as np
+
+def read_wavefile(path):
+    wave_data = wave.open(path, 'r')
+    
+    frames = wave_data.readframes(-1)
+    frames = np.fromstring(frames, 'Int16')
+    
+    params = wave_data.getparams()
+    
+    return frames, params
+
+
+def plot_wave_signal(signal, params):
+    Fs = params[2]
+    T = 1.0/Fs
+    length = len(signal)
+    t = np.arange(0, length, 1)
+    t = t * T
+    
+    plt.figure()
+    plt.title('PCG signal')
+    plt.plot(t, signal)
+    
+    
+filepath = '201101070538.wav'
+signal, params = read_wavefile(filepath)
+plot_wave_signal(signal, params)
