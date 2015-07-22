01333874b009bc572c229a2f7604a1917ecd6d3f
agnieszka-ka <agus.kaczmarczyk@gmail.com>
2015-07-22 14:03:11 +0200

    Refactoring of segmentation_moments.py.

--- segmentation_moments.py
+++ segmentation_moments.py
@@ -7,14 +7,15 @@ Y.Wei
 @author: Agnieszka Kaczmarczyk
 """
 
-delta = 0.0005
-l = 0.005
+delta = 0.05
+l = 0.5
 
 import wave_operations as wo
+import preprocessing as pr
 import numpy as np
 
-def prepare_scale_parameters (params):
-    Fs = params[2]    
+def prepare_scale_parameters (freq):
+    Fs = freq    
     delta = 0.05 * Fs
     l = 0.5 * Fs
     delta = 0.05    # [s]
@@ -22,12 +23,12 @@ def prepare_scale_parameters (params):
     
     return delta, l
     
-def calculate_c(signal, params):
-    start_frame = (int)(l * params[2])
-    stop_frame = params[3] - start_frame
-    delta_frame = (int)(delta * params[2])
+def calculate_c(signal, freq):
+    start_frame = (int)(l * freq)
+    stop_frame = len(signal) - start_frame
+    delta_frame = (int)(delta * freq)
 
-    c = np.zeros(params[3])
+    c = np.zeros(len(signal))
     
     for t in range(start_frame, stop_frame):
 
@@ -39,12 +40,12 @@ def calculate_c(signal, params):
             
     return c
 
-def calculate_I(c, params):
-    start_frame = (int)(l * params[2])
-    stop_frame = params[3] - start_frame
-    delta_frame = (int)(l * params[2])
+def calculate_I(c, freq):
+    start_frame = (int)(l * freq)
+    stop_frame = len(c) - start_frame
+    delta_frame = (int)(l * freq)
     
-    I_moment = np.zeros(params[3]) 
+    I_moment = np.zeros(len(c)) 
     
     for t in range(start_frame, stop_frame):
         
@@ -53,12 +54,12 @@ def calculate_I(c, params):
             
     return I_moment
     
-def calculate_mi(c, I_moment, params):
-    start_frame = (int)(l * params[2])
-    stop_frame = params[3] - start_frame
-    delta_frame = (int)(l * params[2])
+def calculate_mi(c, I_moment, freq):
+    start_frame = (int)(l * freq)
+    stop_frame = len(c) - start_frame
+    delta_frame = (int)(l * freq)
     
-    mi = np.zeros(params[3])    
+    mi = np.zeros(len(c))    
     
     for t in range(start_frame, stop_frame):
         
@@ -71,85 +72,77 @@ def calculate_mi(c, I_moment, params):
         
     return mi
     
-def calculate_I_mi_t_dash(c, params):
-    start_frame = (int)((l+delta) * params[2])
-    stop_frame = params[3] - start_frame
-    delta_l_frame = (int)(l * params[2])
-    delta_frame = (int)(delta * params[2])
+def calculate_I_mi_t_dash(c, freq):
+    start_frame = (int)((l+delta) * freq)
+    stop_frame = len(c) - start_frame
+    delta_l_frame = (int)(l * freq)
+    delta_frame = (int)(delta * freq)
     
-    I_t_dash = np.zeros(params[3])  
-    mi_t_dash = np.zeros(params[3])     
+    I_t_dash = np.zeros(len(c))  
+    mi_t_dash = np.zeros(len(c))     
     
     for t in range(start_frame, stop_frame):
-        
-#        nominator = 0
+    
         denominator = 0
         for tau in range(t - delta_frame, t + delta_frame):
-#            nominator = nominator + tau/params[2] * c[tau]
             denominator = denominator + c[tau]
-#        t_dash = nominator / denominator
         
         for tau in range(t - delta_l_frame, t + delta_l_frame):
-            I_t_dash[t] = I_t_dash[t] + (((tau/params[2] - calculate_t_dash(c, tau))**2) * c[tau])   
+            I_t_dash[t] = I_t_dash[t] + (((tau/freq - calculate_t_dash(c, tau, freq))**2) * c[tau])   
             
         mi_t_dash[t] = I_t_dash[t] / denominator
-        print t
     
     return I_t_dash, mi_t_dash
     
-def calculate_t_dash(c, t):
+def calculate_t_dash(c, t, freq):
     nominator = 0
     denominator = 0
-    delta_frame = (int)(delta * params[2])
+    delta_frame = (int)(delta * freq)
     
     for tau in range(t - delta_frame, t + delta_frame):
-        nominator = nominator + tau/params[2] * c[tau]
+        nominator = nominator + tau/freq * c[tau]
         denominator = denominator + c[tau]
     t_dash = nominator / denominator
     
     return t_dash
         
-def calculate_S(signal, params, t, k):
+def calculate_S(signal, t, k):
     S = 0
-#    print t
     T = np.arange(0, t)
     for tau in T:
         S = S + ((signal[tau])**k)
         
     return S
     
-def calculate_delta_S(signal, params, t, k):
-    delta_frame = (int)(delta * params[2])
+def calculate_delta_S(signal, freq, t, k):
+    delta_frame = (int)(delta * freq)
 
-    return calculate_S(signal, params, (t + delta_frame), k) - calculate_S(signal, params, (t - delta_frame), k)
+    return calculate_S(signal, (t + delta_frame), k) - calculate_S(signal, (t - delta_frame), k)
     
-def calculate_J(signal, params, t, k):
+def calculate_J(signal, freq, t, k):
     J = 0
-    delta_frame = (int)(delta * params[2])
+    delta_frame = (int)(delta * freq)
     
     for tau in range(t - delta_frame, t + delta_frame):
-        J = J + (((tau/params[2])**k) * calculate_delta_S(signal, params, tau, k))
+        J = J + (((tau/freq)**k) * calculate_delta_S(signal, freq, tau, k))
         
     return J / (2 * delta)
     
-def calculate_delta_J(signal, params, t, k):
-    delta_frame = (int)(l * params[2])
+def calculate_delta_J(signal, freq, t, k):
+    delta_frame = (int)(l * freq)
 
-    return calculate_J(signal, params, (t + delta_frame), k) - calculate_J(signal, params, (t - delta_frame), k)
+    return calculate_J(signal, freq, (t + delta_frame), k) - calculate_J(signal, freq, (t - delta_frame), k)
     
-def calculate_I_moment_mi_2(signal, params):
-    start_frame = (int)(2 * l * params[2])
-    stop_frame = params[3] - start_frame
+def calculate_I_moment_mi_2(signal, freq):
+    start_frame = (int)(2 * l * freq)
+    stop_frame = len(signal) - start_frame
 
-    I_moment_2 = np.zeros(params[3])
-    mi_2 = np.zeros(params[3])
+    I_moment_2 = np.zeros(len(signal))
+    mi_2 = np.zeros(len(signal))
     
     for t in range(start_frame, stop_frame):
-        print 'moment'
-        I_moment_2[t] = (calculate_delta_J(signal, params, t, 2)) - (2 * calculate_delta_J(signal, params, t, 1) * (t/params[2])) + (calculate_delta_J(signal, params, t, 0) * ((t/params[2])**2))
-        print 'mi'        
-        mi_2[t] = I_moment_2[t] / (calculate_delta_J(signal, params, t, 0) * (t/params[2]))
-        print t
+        I_moment_2[t] = (calculate_delta_J(signal, freq, t, 2)) - (2 * calculate_delta_J(signal, freq, t, 1) * (t/freq)) + (calculate_delta_J(signal, freq, t, 0) * ((t/freq)**2))  
+        mi_2[t] = I_moment_2[t] / (calculate_delta_J(signal, freq, t, 0) * (t/freq))
     
     return I_moment_2, mi_2
     
