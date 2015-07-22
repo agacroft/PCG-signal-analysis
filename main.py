1c3f14ea77523fdeac37364d991fac5715bd4a08
agnieszka-ka <agus.kaczmarczyk@gmail.com>
2015-07-22 23:00:59 +0200

    Added script for finding the right threshold for Shannon energy envelope based method.

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ Main.
 @author: Agnieszka Kaczmarczyk
 """
 
+import matplotlib.pyplot as plt
 import wave_operations as wo
 import preprocessing as pr
 import segmentation as segm
+import threshold
 
+plt.close('all')
 
 filepath = "103_1305031931979_D2.wav"
+filepath = "201101151127.wav"
 signal_PCG, params = wo.read_wavefile(filepath)
 freq = 4000
 
@@ -26,4 +30,6 @@ signal_PCG = pr.butter_lowpass_filter(signal_PCG, cutoff, freq, 1)
 
 # Shannon energy envelope
 shannon_envelope = segm.shannon_energy_envelope(signal_PCG, freq)
-wo.plot_wave_signal(shannon_envelope, freq)
+#wo.plot_wave_signal(shannon_envelope, freq)
+
+threshold.determine_threshold(shannon_envelope, freq)
