aa0ab2bb3fc81bcb47307d594b5cd045dffe06fb
agnieszka-ka <agus.kaczmarczyk@gmail.com>
2015-07-22 14:05:12 +0200

    Added filtering methods to preprocessing.py.

--- preprocessing.py
+++ preprocessing.py
@@ -6,9 +6,12 @@ Preprocessing of PCG data.
 """
 
 import numpy as np
-import wave_operations
+import wave_operations as wo
+from scipy.signal import butter, lfilter
+import scipy.fftpack
+import matplotlib.pyplot as plt
 
-def decimate(signal, params, f_k):
+def decimate(signal_in, params, f_k):
     f_p = params[2]
     n = (int)(f_p / f_k)
     signal_len = (int)(params[3] / n)
@@ -16,9 +19,39 @@ def decimate(signal, params, f_k):
     signal_out = np.zeros(signal_len)
     
     for index in range(0, signal_len):
-        signal_out[index] = signal[index * n]
+        signal_out[index] = signal_in[index * n]
         
     return signal_out
     
-def normalize(signal):
-    return signal * 1.0 / max(abs(signal))
+def normalize(signal_in):
+    return signal_in * 1.0 / max(abs(signal_in))
+
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+def butter_lowpass_filter(data, cutoff, fs, order=1):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+def butter_bandpass(lowcut, highcut, fs, order=1):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+ 
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+def show_fft(signal_in, params):
+    FFT = abs(scipy.fft(signal_in[0: params[2] - 1], params[2]))
+    freq = scipy.fftpack.fftfreq(params[3], 1.0/params[2])
+
+    plt.plot(freq[0: params[2]/2], abs(FFT[0: params[2]/2]))
+
