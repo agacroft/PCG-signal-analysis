92e9a6f7decfc80d105cfc6e89c9b25da21e7c5b
agnieszka-ka <agus.kaczmarczyk@gmail.com>
2015-07-27 13:21:23 +0200

    Created script for determining features of PCG signals.

---
+++ parametrization.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Script for PCG signal parametrization.
+Used parameters:
+- FFT for S1 - not yet
+- FFT for S2 - not yet
+- FFT for other peaks detected - not yet
+- Wigner distribution of tones - not yet, not sure
+- STFT on whole signal - not yet
+- total power during systole - not yet
+- Q-factor during systole - not yet
+- t1 - S1 durarion time - not yet
+- t12 - time between end of S1 and start of S2 - not yet
+- mean12 - maximum of means in regions between S1 - S2 and S2 - S1 - not yet
+
+@author: Agnieszka Kaczmarczyk
+"""
+
