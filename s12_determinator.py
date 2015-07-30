9bf2ffcee7550af210f5702afcb4f5975af71d6c
agnieszka-ka <agus.kaczmarczyk@gmail.com>
2015-07-30 22:13:27 +0200

    Included possibility of having not identified peak during the cycle in peaks determination.

--- s12_determinator.py
+++ s12_determinator.py
@@ -6,6 +6,8 @@ Script for determining S1 and S2 peaks.
 """
 
 import numpy as np
+import preprocessing as pr
+import matplotlib.pyplot as plt
 
 def find_cycle_start(signal_in, starts, heart_rate, freq):
     cycle_frames = int(60.0 / heart_rate * freq)
@@ -100,6 +102,7 @@ def determine_s12_with_type_1(signal_original, starts, stops, boundaries, peaks_
     
     s1 = []
     s2 = []
+    s12 = []
     for range_index in range(0, ranges_n):
         range_start = boundaries[range_index] - int(0.1 * 60.0 / heart_rate * freq)
         range_stop = boundaries[range_index + 1] + int(0.1 * 60.0 / heart_rate * freq)
@@ -110,7 +113,6 @@ def determine_s12_with_type_1(signal_original, starts, stops, boundaries, peaks_
         i = 0
         while start_index < 0 and i < peaks_n:
             if starts_search[i] > range_start:
-                print 'start: ' + str(i)
                 start_index = i
             else:
                 i = i + 1
@@ -118,19 +120,82 @@ def determine_s12_with_type_1(signal_original, starts, stops, boundaries, peaks_
         i = 1 
         while stop_index < 0 and i < peaks_n:
             if stops_search[i] > range_stop and start_index < i:
-                print 'stop: ' + str(i)
                 stop_index = i - 1
             else:
                 i = i + 1
    
         if stop_index - start_index >= 1:
+            s1_index = np.argmax(peaks_weights[start_index : stop_index + 1], axis = 0) + start_index  
+            s1.append(s1_index)
+            peaks_weights[s1_index] = -2
+            s2_index = np.argmax(peaks_weights[start_index : stop_index + 1], axis = 0) + start_index           
+            s2.append(s2_index)
+            peaks_weights[s2_index] = -2
+        elif start_index == stop_index and start_index > -1:
+            s12.append(start_index)
+            peaks_weights[start_index] = -2
+        
+    return s1, s2, s12
+    
+# Determining s1, s2 based also on type of signal.
+def determine_s1_with_type_2(starts, stops, boundaries, peaks_energy, heart_rate, freq):
+    ranges_n = len(boundaries) - 1
+    
+    peaks_lengths = [b - a for a,b in zip(starts, stops)]
+    peaks_weights = [0.3 * e + 0.7 * l for e,l in zip(peaks_energy, peaks_lengths)]    
+    
+    starts_search = np.copy(starts)
+    stops_search = np.copy(stops)    
+    
+    starts_search = np.append(starts_search, np.array([boundaries[len(boundaries) - 1] + int(0.1 * 60.0 / heart_rate * freq) + 2]))  
+    stops_search = np.append(stops_search, np. array([boundaries[len(boundaries) - 1] + int(0.1 * 60.0 / heart_rate * freq) + 4]))
+    
+    peaks_n = len(starts_search)
+    
+    s1 = []
+    
+    for range_index in range(0, ranges_n):
+        range_start = boundaries[range_index] - int(0.1 * 60.0 / heart_rate * freq)
+        range_stop = boundaries[range_index + 1] + int(0.1 * 60.0 / heart_rate * freq)
+        # Finding indexes of peaks in one cycle.
+        start_index = -1
+        stop_index = -1
+        
+        i = 0
+        while start_index < 0 and i < peaks_n:
+            if starts_search[i] > range_start:
+                start_index = i
+            else:
+                i = i + 1
+                
+        i = 1 
+        while stop_index < 0 and i < peaks_n:
+            if stops_search[i] > range_stop and start_index < i:
+                stop_index = i - 1
+            else:
+                i = i + 1
+   
+        if stop_index - start_index >= 0:
             s1_index = np.argmax(peaks_weights[start_index : stop_index + 1], axis = 0) + start_index        
             s1.append(s1_index)
             peaks_weights[s1_index] = 0
-            s2_index = np.argmax(peaks_weights[start_index : stop_index + 1], axis = 0) + start_index     
-            s2.append(s2_index)
-            peaks_weights[s2_index] = 0
         
-    return s1, s2
+    return s1
     
-# Determining s1, s2 based also on type of signal.
+def peaks_fft_parameters(signal, starts, stops, freq):
+    n = len(starts)
+    peaks_max_frequencies = []
+    peaks_spectral_centroids = []
+    for peak_index in range(0, n):
+        peak_length_accuracy = int(0.1 * (stops[peak_index] - starts[peak_index]))
+        peak_start = starts[peak_index] #+ peak_length_accuracy
+        peak_stop = stops[peak_index]# + peak_length_accuracy
+        FFT, frequencies = pr.fft_freq(signal[peak_start : peak_stop], freq)
+        peaks_max_frequencies.append(max(FFT))
+        peaks_spectral_centroids.append(pr.spectral_centroid(FFT[0: len(FFT)/2], frequencies[0: len(FFT)/2]))
+        
+#        plt.figure()
+#        plt.plot(frequencies[0: len(FFT)/2], FFT[0: len(FFT)/2])
+#        plt.title(str(peak_index))
+    
+    return peaks_max_frequencies, peaks_spectral_centroids
